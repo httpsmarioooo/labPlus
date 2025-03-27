ac3cc12e7c3f134615292f6e2291834e9908d7e0
Carlos Mario <carlosmariobig14@gmail.com>
2025-03-26 19:41:30 -0500

    Tareas hechas

---
+++ task3.js
@@ -0,0 +1,13 @@
+// Refer to Task 3 in your Instructions to complete this task
+
+for (let i = 1; i <= 105; i++) {
+  if (i % 15 == 0){
+    console.log("FizzBuzz")
+  }else if (i % 3 == 0){
+    console.log("Fizz")
+  }else if (i % 5 == 0){
+    console.log("Buzz")
+  }else{
+  console.log(i)
+  }
+};
